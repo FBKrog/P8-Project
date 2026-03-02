e5f657c4feae83f862f4a4b967a14a523311485e
Akaidus <nikolajjensen6854@gmail.com>
2026-03-02 20:54:32 +0100

    DAOM arm
    + Some rotation shenanigans.
    + Cute robot arm instead of Kyle's body.
    + Can now pickup and bring back interactables immediatly when they are hit with the DAOM arm.
    + Can now transport interactables back and forth.
    + VR Controller Inputs for aim and launch/recall.
    
    * BUG to be fixed: Interactables do not snap to players hand if they don't "re-grab" the interactable before hand.

--- P8 Unity Project/Assets/Niko/DAOMArm.cs
+++ P8 Unity Project/Assets/Niko/DAOMArm.cs
@@ -1,51 +1,127 @@
-using System.Collections;
-using System.Drawing;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class DAOMArm : MonoBehaviour
 {
     [Header("Player")]
     [SerializeField] GameObject playerRoot;
-    [SerializeField] GameObject playerIKTarget;
+    [SerializeField] GameObject playerIKTarget; // Called player hand in comments for readability
 
     [Header("DAOM")]
     [SerializeField] GameObject daomRoot;
-    [SerializeField] GameObject daomIKTarget;
-    [SerializeField] float travelTime = 1f;
-    [SerializeField] float rotationTime = 0.5f;
+    [SerializeField] GameObject daomIKTarget; // Called daom hand in comments for readability
+    [SerializeField] Animator RigAnimator;
+    [SerializeField] GameObject extendedArmPart;
 
-    Quaternion targetRot;
+    [Header("Travel")]
+    [SerializeField] [Tooltip("The time it takes the arm to travel from the body to the surface and vice-versa.")] float travelDuration = 1f;
+
+
+    [Header("Rotation")]
+    [SerializeField] GameObject lowerArm;
+    [SerializeField] [Tooltip("The point in travel time the arm will rotate, normalized from percentage of traveled distance")] float rotationStartTime = 0.8f;
+    [SerializeField] [Tooltip("The time it takes the arm to rotate into place relative to the surface's normal.")] float rotationDuration = 0.5f;
+    [SerializeField] Vector3 rotationOffset = new(0, 180,90);
+
+    [Header("Other")]
+    [SerializeField] [Tooltip("Make the arm act as if mirrored.")] bool mirror = true;
+
+    [Header("Interactor")]
+    [SerializeField] XRDirectInteractor interactor;
+
     Quaternion startRot;
+    Quaternion targetRot;
 
-    bool isTraveling = false;
+    Quaternion lowerArmStartRot;
+    int dumbfix = 0;
+    IXRSelectInteractable selectedInteractable;
 
-    bool isAttachedToSurface = false;
+
+    [SerializeField]bool isTraveling = false;
+    [SerializeField]bool isAttachedToSurface = false;
     public bool IsAttachedToSurface => isAttachedToSurface;
-
     bool recalling = false;
     public bool Recalling => recalling;
 
-    public void Initialize(GameObject root, GameObject IKTarget, Vector3 point, Vector3 normal)
+    void OnEnable()
+    {
+        LaunchArm.SetInteractorHandedness += GetHandedness;
+        interactor.selectEntered.AddListener(OnGrab);
+        interactor.selectExited.AddListener(args => selectedInteractable = null);
+    }
+
+    void OnDisable()
+    {
+        LaunchArm.SetInteractorHandedness -= GetHandedness;
+    }
+
+    void GetHandedness(XRDirectInteractor interactor)
+    {
+        this.interactor.handedness = interactor.handedness;
+        this.interactor.selectInput = interactor.selectInput;
+        this.interactor.activateInput = interactor.activateInput;
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        selectedInteractable = args.interactableObject;
+    }
+
+    /// <summary>
+    /// Initializes the player by setting the root and inverse kinematics (IK) target, and begins movement toward the
+    /// specified point.
+    /// </summary>
+    public void Initialize(GameObject root, GameObject IKTarget, Vector3 point, Vector3 normal, IXRSelectInteractable hitInteractable = null, IXRSelectInteractable interactable = null)
     {
         playerRoot = root;
         playerIKTarget = IKTarget;
-        StartCoroutine(TravelToPoint(point, normal));
+
+        RigAnimator.enabled = false;
+        extendedArmPart.SetActive(false);
+
+        if(interactor != null && interactable != null)
+        {
+            interactor.interactionManager.SelectEnter(interactor, interactable);
+            selectedInteractable = interactable;
+            interactor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.Sticky;
+        }
+
+        StartCoroutine(TravelToPoint(point, normal, hitInteractable));
     }
 
-    public void RecallArm(Vector3 point, Vector3 normal)
+    /// <summary>
+    /// Initiates the process of recalling the arm to the specified point and orientation.
+    /// </summary>
+    public void RecallArm(GameObject goPoint, Vector3 normal)
     {
         if (isTraveling) return;
         if (!isAttachedToSurface) return;
         if (recalling) return;
         recalling = true;
-        StartCoroutine(TravelToPoint(point, normal));
-        StartCoroutine(RotateToNormal());
+
+        interactor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.Sticky;
+        LaunchArm.OnGrabbedGameObject(selectedInteractable);
+        
+        StartCoroutine(TravelToGameObject(goPoint, normal));
+        StartCoroutine(RotateToNormal(transform, startRot, targetRot, rotationDuration));
     }
 
-    IEnumerator TravelToPoint(Vector3 point, Vector3 normal)
+    /// <summary>
+    /// Moves the object smoothly to the specified POSITION while storing the given surface normal in a variable for later use.
+    /// </summary>
+    IEnumerator TravelToPoint(Vector3 point, Vector3 normal, IXRSelectInteractable interactable)
     {
         if (isTraveling) yield break;
         isTraveling = true;
+
+        selectedInteractable = interactable;
+        
+        extendedArmPart.SetActive(false);
+
         Vector3 startPos = transform.position;
 
         startRot = transform.rotation;
@@ -54,9 +130,13 @@ public class DAOMArm : MonoBehaviour
         float elapsedTime = 0f;
         while (true) {
             elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / travelTime);
+            float t = Mathf.Clamp01(elapsedTime / travelDuration);
+            if (t >= rotationStartTime)
+            {
+                PrepareSurfaceLanding();
+            }
             transform.position = Vector3.Lerp(startPos, point, t);
-            if (elapsedTime >= travelTime) 
+            if (elapsedTime >= travelDuration) 
             {
                 ArmAttaching();
                 break;
@@ -66,32 +146,100 @@ public class DAOMArm : MonoBehaviour
         yield break;
     }
 
+    /// <summary>
+    /// Moves the object smoothly to the specified GAMEOBJECT while storing the given surface normal in a variable for later use.
+    /// </summary>
+    IEnumerator TravelToGameObject(GameObject goPoint, Vector3 normal)
+    {
+        if (isTraveling) yield break;
+        isTraveling = true;
+
+        extendedArmPart.SetActive(false);
+
+        Vector3 startPos = transform.position;
+
+        startRot = transform.rotation;
+        targetRot = Quaternion.LookRotation(normal);
+
+        float elapsedTime = 0f;
+        while (true)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / travelDuration);
+            if (t >= rotationStartTime)
+            {
+                PrepareSurfaceLanding();
+            }
+            transform.position = Vector3.Lerp(startPos, goPoint.transform.position, t);
+            if (elapsedTime >= travelDuration)
+            {
+                ArmAttaching();
+                break;
+            }
+            yield return null;
+        }
+        yield break;
+    }
+
+    void PrepareSurfaceLanding()
+    {
+        if(dumbfix < 1)
+        {
+            lowerArmStartRot = lowerArm.transform.rotation;
+            var targetRotation = Quaternion.Euler(new Vector3(90,0,0)); // The arm model is rotated funky, so this is a hardcoded fix to make it look like the arm is bending at the elbow as it approaches the surface. This is really scuffed and should be replaced with a better solution ¯\_(ツ)_/¯
+            StartCoroutine(RotateToNormal(lowerArm.transform, lowerArmStartRot, targetRotation, rotationDuration));
+            dumbfix++;
+        }
+    }
+
+    /// <summary>
+    /// Attaches the arm to a surface and initiates alignment to the surface normal.
+    /// </summary>
     void ArmAttaching()
     {
         isTraveling = false;
         isAttachedToSurface = true;
-        StartCoroutine(RotateToNormal());
+
+        // If the arm hit an interactable, recall the arm WITH the interactable so the player holds it after recall.
+        if (selectedInteractable != null && !recalling)
+        {
+            LaunchArm.OnEarlyRecall();
+            interactor.interactionManager.SelectEnter(interactor, selectedInteractable);
+            return;
+        }
+
+        extendedArmPart.SetActive(true);
+
+        StartCoroutine(RotateToNormal(gameObject.transform, startRot, targetRot, rotationDuration));
         if (recalling)
         {
-            LaunchArm.ArmRecalled();
+            LaunchArm.OnArmRecalled();
             return;
         }
     }
 
-    IEnumerator RotateToNormal()
+    /// <summary>
+    /// Rotates the object from its current orientation to the target rotation over a specified duration.
+    /// </summary>
+    IEnumerator RotateToNormal(Transform transform, Quaternion startRot, Quaternion targetRot, float duration)
     {
         float elapsedTime = 0f;
         while (true)
         {
             elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / rotationTime);
+            float t = Mathf.Clamp01(elapsedTime / duration);
             transform.rotation = Quaternion.Slerp(startRot, targetRot, t);
-            if (elapsedTime >= travelTime)
+            if (elapsedTime >= duration)
             {
                 break;
             }
             yield return null;
         }
+
+        // When arm is good to go, enable animator and set the interactor to act normally so the player can grab things with it.
+        interactor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.StateChange;
+        RigAnimator.enabled = true;
+
         yield break;
     }
 
@@ -102,17 +250,60 @@ public class DAOMArm : MonoBehaviour
         RotateToPlayerHand();
     }
 
+    /// <summary>
+    /// Translates the position of the DAOM hand target to match the relative position of the player hand, optionally
+    /// mirroring it across the body.
+    /// </summary>
     void TransformToPlayerHand()
     {
         // Get the position of the player hand relative to the player root, and apply that same relative position to the daom root to find the position for the daom hand.
         Vector3 playerHandOffset = playerRoot.transform.InverseTransformPoint(playerIKTarget.transform.position);
-        daomIKTarget.transform.position = daomRoot.transform.TransformPoint(playerHandOffset);
+
+        if(mirror)
+        {
+            // Mirror on x-axis.
+            playerHandOffset.x *= -1;
+            //playerHandOffset.z *= -1; // Mirror on z-axis will 
+        }
+
+        // Original solution that doesn't compensate for scaled daom parent :( 
+        //daomIKTarget.transform.position = daomRoot.transform.TransformPoint(playerHandOffset);
+
+        // Store the offset from the daom root to world space to find the target position for the daom hand.
+        Vector3 worldOffset = daomRoot.transform.rotation * playerHandOffset;
+
+        // Compensate for scaled parent hierarchy (VERY BAD BTW)
+        Vector3 parentScale = daomIKTarget.transform.parent.lossyScale;
+
+        worldOffset = new Vector3(
+            worldOffset.x / parentScale.x,
+            worldOffset.y / parentScale.y,
+            worldOffset.z / parentScale.z
+            );
+
+        daomIKTarget.transform.position = daomRoot.transform.position + worldOffset;
     }
 
+    /// <summary>
+    /// Rotates the DAOM hand to match the orientation of the player hand relative to their respective root transforms.
+    /// </summary>
     void RotateToPlayerHand()
     {
         // Get the rotation of the player hand relative to the player root, and apply that same relative rotation to the daom root to find the rotation for the daom hand.
-        Quaternion relativeRot = Quaternion.Inverse(playerRoot.transform.rotation) * playerIKTarget.transform.rotation;
+        Quaternion relativeRot = Quaternion.Inverse(playerRoot.transform.rotation) * playerIKTarget.transform.rotation * Quaternion.Euler(rotationOffset);
+
+        if(mirror)
+        {
+            // Mirror on y-axis.
+            //relativeRot = Quaternion.AngleAxis(180f, Vector3.up) * relativeRot;
+            
+            Vector3 euler = relativeRot.eulerAngles;
+            euler.y *= -1;
+            euler.z *= -1;
+            relativeRot = Quaternion.Euler(euler);
+        }
+
+        // Apply the relative rotation to the daom root to find the target rotation for the daom hand.
         daomIKTarget.transform.rotation = daomRoot.transform.rotation * relativeRot;
     }
 }
--- P8 Unity Project/Assets/Niko/LaunchArm.cs
+++ P8 Unity Project/Assets/Niko/LaunchArm.cs
@@ -1,5 +1,10 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class LaunchArm : MonoBehaviour
 {
@@ -13,25 +18,115 @@ public class LaunchArm : MonoBehaviour
     [SerializeField] GameObject armIKTarget;
 
     [Header("Launched Arm")]
-    [SerializeField] GameObject LaunchedArmPrefab;
+    [SerializeField] GameObject daomArmPrefab;
     [SerializeField] GameObject launchPoint;
+    [SerializeField] [Tooltip("The rotation of which the DAOM prefab is launched")] Vector3 launchRotation = new Vector3(-180, -90, 0);
     GameObject daomArm;
 
-    bool canLaunch = true;
+    [Header("Interactor")]
+    [SerializeField] XRDirectInteractor interactor;
 
-    public static Action OnArmRecalled;
-    public static void ArmRecalled() => OnArmRecalled?.Invoke();
+    [Header("Input")]
+    [SerializeField] InputActionReference launchInput;
+    [SerializeField] InputActionReference aimInput;
+
+    [Header("State")]
+    [SerializeField] bool aiming = false;
+    [SerializeField] bool canLaunch = true;
+
+    LineRenderer lineRenderer;
+
+    IXRSelectInteractable carriedInteractable;
+    IXRSelectInteractable daomInteractable;
+    IXRSelectInteractable hitInteractable;
+
+    public static Action<XRDirectInteractor> SetInteractorHandedness;
+    public static void OnSetInteractorHandedness(XRDirectInteractor interactor) => SetInteractorHandedness?.Invoke(interactor);
+
+    public static Action ArmRecalled;
+    public static void OnArmRecalled() => ArmRecalled?.Invoke();
+
+    public static Action<IXRSelectInteractable> GrabbedGameObject;
+    public static void OnGrabbedGameObject(IXRSelectInteractable interactable) => GrabbedGameObject?.Invoke(interactable);
+
+    public static Action EarlyRecall;
+    public static void OnEarlyRecall() => EarlyRecall?.Invoke();
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
 
     void OnEnable()
     {
-        OnArmRecalled += RemoveDAOMArm;
+        ArmRecalled += RemoveDAOMArm;
+        EarlyRecall += canLaunch ? Launch : null;
+        GrabbedGameObject += AddGrabbedGameObject;
+
+        // Input
+        interactor.selectExited.AddListener(args => carriedInteractable = null);
+        interactor.selectEntered.AddListener(OnGrab);
+        
+        launchInput.action.performed += ctx => Launch();
+        
+        aimInput.action.performed += AimState;
+        aimInput.action.canceled += AimState;
     }
+
 
     void OnDisable()
     {
-        OnArmRecalled -= RemoveDAOMArm;
+        ArmRecalled -= RemoveDAOMArm;
+        GrabbedGameObject -= AddGrabbedGameObject;
+        EarlyRecall -= canLaunch ? Launch : null;
+
+        // Input
+        interactor.selectEntered.RemoveListener(OnGrab);
+        interactor.selectExited.RemoveListener(args => carriedInteractable = null);
+
+        launchInput.action.performed -= ctx => Launch();
+        
+        aimInput.action.performed -= AimState;
+        aimInput.action.canceled -= AimState;
     }
 
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        carriedInteractable = args.interactableObject;
+    }
+
+    /// <summary>
+    /// Stores the currently grabbed object, if any, to make sure the player will hold that object after recalling the arm.
+    /// </summary>
+    /// <param name="gameObject"></param>
+    void AddGrabbedGameObject(IXRSelectInteractable gameObject)
+    {
+        if (gameObject == null)
+        {
+            daomInteractable = null;
+            return;
+        }
+        daomInteractable = gameObject;
+    }
+
+    void AimState(InputAction.CallbackContext ctx)
+    {
+        if(!canLaunch) return;
+        if (ctx.ReadValue<float>() > 0)
+        {
+            interactor.keepSelectedTargetValid = false;
+            aiming = true;
+        }
+        else
+        {
+            interactor.keepSelectedTargetValid = true;
+            aiming = false;
+        }
+    }
+
+    /// <summary>
+    /// Removes the currently active DAOM arm from the scene, if one exists. And resets the state to allow for launching again as well as interaction.
+    /// </summary>
     void RemoveDAOMArm()
     {
         if (daomArm != null)
@@ -39,7 +134,19 @@ public class LaunchArm : MonoBehaviour
             Destroy(daomArm);
             daomArm = null;
             canLaunch = true;
+            ForceGrabInteractable();
         }
+    }
+
+    void ForceGrabInteractable()
+    {
+        interactor.enabled = true;
+        if (daomInteractable != null)
+        {
+            interactor.interactionManager.SelectEnter(interactor, daomInteractable);
+        }
+        daomInteractable = null;
+        hitInteractable = null;
     }
 
     void Update()
@@ -47,13 +154,17 @@ public class LaunchArm : MonoBehaviour
         ValidLayer();
     }
 
+    void LateUpdate()
+    {
+        DrawAimLine();
+    }
+
     /// <summary>
     /// Shoot a ray forward to check for valid surfaces
     /// </summary>
-    /// <returns></returns>
     bool ValidLayer()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hit, rayLength, surfaceLayer))
+        if (aiming && Physics.Raycast(transform.position, transform.forward, out hit, rayLength, surfaceLayer))
         {
             return true;
         }
@@ -67,28 +178,37 @@ public class LaunchArm : MonoBehaviour
     {
         if (!canLaunch)
         {
-            ResetArm();
+            RecallArm();
         }
         if(canLaunch && ValidLayer())
         {
-            if(daomArm != null)
+            if (daomArm != null)
             {
                 if(!daomArm.GetComponent<DAOMArm>().Recalling)
                 {
                     Debug.Log("Arm is recalling, cannot launch!");
-                    return;
                 }
+                return;
             }
+            // Preconditions met, launch the arm and set canLaunch to false until the arm is recalled.
             canLaunch = false;
-            if(daomArm == null)
+            aiming = false;
+            interactor.keepSelectedTargetValid = true;
+            if (hit.collider.gameObject.transform.parent.TryGetComponent(out XRGrabInteractable interactable) && carriedInteractable == null)
             {
-                daomArm = Instantiate(LaunchedArmPrefab, launchPoint.transform.position, launchPoint.transform.rotation);
-                daomArm.GetComponent<DAOMArm>().Initialize(armRoot, armIKTarget, hit.point, hit.normal);
+                hitInteractable = interactable;
             }
+            daomArm = Instantiate(daomArmPrefab, launchPoint.transform.position, Quaternion.Euler(launchRotation));
+            daomArm.GetComponent<DAOMArm>().Initialize(armRoot, armIKTarget, hit.point, hit.normal, hitInteractable, carriedInteractable);
+            OnSetInteractorHandedness(interactor);
+            interactor.enabled = false;
         }
     }
 
-    void ResetArm()
+    /// <summary>
+    /// Recalls the arm to the launch point if it is currently attached to a surface.
+    /// </summary>
+    void RecallArm()
     {
         if (daomArm != null)
         {
@@ -97,8 +217,20 @@ public class LaunchArm : MonoBehaviour
                 Debug.Log("Arm is not attached to surface, cannot reset!");
                 return;
             }
-            daomArm.GetComponent<DAOMArm>().RecallArm(launchPoint.transform.position, launchPoint.transform.forward);
+            daomArm.GetComponent<DAOMArm>().RecallArm(launchPoint, launchPoint.transform.forward);
         }
+    }
+
+    void DrawAimLine()
+    {
+        if(ValidLayer() && daomArm == null)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.point);
+            return;
+        }
+        lineRenderer.enabled = false;
     }
 
     void OnDrawGizmos()
