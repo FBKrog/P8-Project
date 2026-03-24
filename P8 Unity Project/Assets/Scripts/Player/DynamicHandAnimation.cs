ea486962d132aeeae8b6a837b2636aa2214a0fc7
Akaidus <nikolajjensen6854@gmail.com>
2026-03-24 14:48:11 +0100

    Hand Animation System
    + Draft version of "animation" system for hand/fingers, dependant on vector values.
    The idea is that each object instructs the fingers on where they should be placed.
    * Maybe replace with IK system instead of vectors.

---
+++ P8 Unity Project/Assets/Scripts/Player/DynamicHandAnimation.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class DynamicHandAnimation : MonoBehaviour
+{
+    [SerializeField] HandData handData;
+
+    void Awake()
+    {
+        if (handData == null)
+        {
+            Debug.LogError("Hand data is not assigned or empty.");
+            return;
+        }
+        for (int i = 0; i < 2; i++)
+            handData.thumb.jointValues[i] = SetInitialPhalanx(handData.thumb.joints[i]);
+        for (int i = 0; i < 3; i++)
+        {
+            handData.index.jointValues[i] = SetInitialPhalanx(handData.index.joints[i]);
+            handData.middle.jointValues[i] = SetInitialPhalanx(handData.middle.joints[i]);
+            handData.ring.jointValues[i] = SetInitialPhalanx(handData.ring.joints[i]);
+            handData.pinky.jointValues[i] = SetInitialPhalanx(handData.pinky.joints[i]);
+        }
+    }
+    void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        Animate();
+#endif
+    }
+
+    void Animate()
+    {
+        for (int i = 0; i < 2; i++)
+            BendPhalanx(handData.thumb.joints[i], handData.thumb.jointValues[i]);
+        for (int i = 0; i < 3; i++)
+        {
+            BendPhalanx(handData.index.joints[i], handData.index.jointValues[i]);
+            BendPhalanx(handData.middle.joints[i], handData.middle.jointValues[i]);
+            BendPhalanx(handData.ring.joints[i], handData.ring.jointValues[i]);
+            BendPhalanx(handData.pinky.joints[i], handData.pinky.jointValues[i]);
+        }
+    }
+
+    void BendPhalanx(Transform phalanx, Vector3 value)
+    {
+        phalanx.localRotation = Quaternion.Euler(value.x, value.y, value.z);
+    }
+
+    Vector3 SetInitialPhalanx(Transform phalanx)
+    {
+        return phalanx.localRotation.eulerAngles;
+    }
+}
+
+[System.Serializable]
+public class HandData
+{
+    public FingerData thumb;
+    public FingerData index;
+    public FingerData middle;
+    public FingerData ring;
+    public FingerData pinky;
+}
+
+[System.Serializable]
+public class FingerData
+{
+    public Transform[] joints;
+    public Vector3[] jointValues;
+}
